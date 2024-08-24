15768ec354398d02b59246d24dc259b044e1d524
dvita-ilmar <dvita@mail.ru>
2024-08-24 13:33:48 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+Дегтярев Виталий (группа 22/08)
+Домашнее задание №2
+Практическое задание по работе в Pycharm - "Переменные".
+'''
+number_of_compl_hw = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent*60/number_of_compl_hw #В минутах!
+print(f'Курс: {course_name}, всего задач:{number_of_compl_hw}, затрачено часов: {number_of_hours_spent}, \
+среднее время выполнения {time_for_one_task} минут')
